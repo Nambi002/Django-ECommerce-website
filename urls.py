d5947b1c070880636837dc1c223c112b2af08313
Nambivelnathan <snambivelnathan@gmail.com>
2024-12-21 20:00:18 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -14,17 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
-
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from .import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('web.urls')),
+    path('',views.home,name="home"),
+    path('register/',views.register,name="register"),
+    path('login/',views.login_page,name="login"),
+    path('logout/',views.logout_page,name="logout"),
+    path('cart',views.cart_page,name="cart"),
+    path('fav',views.fav_page,name="fav"),
+    path('favviewpage',views.favviewpage,name="favviewpage"),
+    path('remove_fav/<str:fid>',views.remove_fav,name="remove_fav"),
+    path('remove_cart/<str:cid>',views.remove_cart,name="remove_cart"),
+    path('collections',views.collections,name="collections"),
+    path('collections/<str:name>/',views.collectionsview,name="collections"),
+    path('collections/<str:name>/<str:pname>',views.product_details,name="product_details"), 
+    path('addtocart',views.add_to_cart,name="addtocart"),
+
 ]
-
-
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
